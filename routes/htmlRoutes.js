5ee011cfc61db9185819948e8c8fcf05be14fa5b
Somelina Obiechina <ifeasome@gmail.com>
2021-02-05 15:38:14 -0600

    Create htmlRoutes.js

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,28 @@
+// Dependencies
+const router = require("express").Router(); 
+const path = require("path");
+
+
+
+// a res.send file 
+// a join directory of the current path to whereever the html file is stored 
+// DIRNAME 
+// NEEDS TO BE EXPORTED into the server.js 
+// in the server use the routes folder. 
+
+module.exports = function(router) {
+ 
+    router.get("/exercise", function(req, res) {
+        res.sendFile(path.join(__dirname, "./public/exercise.html"));
+    }); 
+    // if no matching routes are found
+    router.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "./public/index.html"));
+    });
+
+    router.get("/stats", function(req, res) {
+        res.sendFile(path.join(__dirname, "./public/stats.html"));
+    });
+}
+
+
